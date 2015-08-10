9c0e0755832a0afcc8ef98a65fd7f16b9ac22cd1
Anthony "Two Scoops" <abuzzlife@gmail.com>
2015-08-10 16:22:46 -0400

    Update everythingbetrue.js

--- basic-algorithms/everythingbetrue.js
+++ basic-algorithms/everythingbetrue.js
@@ -12,4 +12,5 @@ function every(collection, pre) {
   
 }
 
-every([{'user': 'Tinky-Winky', 'sex': 'male'}, {'user': 'Dipsy', 'sex': 'male'}, {'user': 'Laa-Laa', 'sex': 'female'}, {'user': 'Po', 'sex': 'female'}], 'sex');
+every([{'user': 'Tinky-Winky', 'sex': 'male'}, {'user': 'Dipsy', 'sex': 'male'}, 
+{'user': 'Laa-Laa', 'sex': 'female'}, {'user': 'Po', 'sex': 'female'}], 'sex');
