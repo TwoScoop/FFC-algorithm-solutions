73a7cee714c6985077d6fedac6085ebaa75dbcc4
Anthony Buzzetta <abuzzlife@gmail.com>
2015-08-10 13:45:53 -0400

    Sorted Union solution

--- basic-algorithms/sortedunion.js
+++ basic-algorithms/sortedunion.js
@@ -1,5 +1,5 @@
 function unite(arr1, arr2, arr3) {  
-  
+  //Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
   for (var i in arr2) {
     if (arr1.indexOf(arr2[i]) === -1) arr1.push(arr2[i]);
   }
