a10bd808624b97c81d1bf7dfa2eb09b41d929e0d
Anthony Buzzetta <abuzzlife@gmail.com>
2015-08-10 16:22:04 -0400

    Everything Be True solution

---
+++ basic-algorithms/everythingbetrue.js
@@ -0,0 +1,15 @@
+function every(collection, pre) {
+  /* Check if the predicate (second argument) returns truthy 
+  (defined) for all elements of a collection (first argument). */
+  
+  for(i=0; i<collection.length; i++) {
+      if(collection[i].hasOwnProperty(pre) === false) {
+        return false;            
+      } 
+  }
+  
+  return true; 
+  
+}
+
+every([{'user': 'Tinky-Winky', 'sex': 'male'}, {'user': 'Dipsy', 'sex': 'male'}, {'user': 'Laa-Laa', 'sex': 'female'}, {'user': 'Po', 'sex': 'female'}], 'sex');
